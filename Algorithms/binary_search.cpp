621c54c0053c27f188369979205041964912e86d
Harsh Prasad <harshprasad1204@gmail.com>
2025-04-19 10:46:47 +0530

    Learn Binary Search

--- Algorithms/binary_search.cpp
+++ Algorithms/binary_search.cpp
@@ -2,34 +2,34 @@
 #include<vector>
 using namespace std;
 
-int binarySearch(vector<int> arr, int key) {
-    int high = arr.size() - 1; // Fix high index
-    int low = 0;
+int binarySearch(vector<int>arr , int search){
 
-    if (arr.size() <= 1) {
-        return (arr[0] == key) ? 0 : -1;
+    int high = arr.size()-1;   // if high = arr.size();  then high value == arr.size ; and the actual index in size-1;
+    int low=0;
+    
+    // Edge case
+    if(arr.size()<=1){
+        return ( arr[0]==search) ? 0 :-1;
     }
 
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-        if (arr[mid] > key) {
-            high = mid - 1;
-        } else if (arr[mid] < key) {
-            low = mid + 1;
-        } else {
+    while(low<=high){
+        int mid = low+ (high -low)/2;
+        if(arr[mid]> search){
+            high=mid-1;
+        }else if(arr[mid]<search){
+            low=mid+1;
+        }else{
             return mid;
         }
     }
 
-    return -1; // Key not found
+    return -1;
+
 }
 
-int main() {
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
-    int key = 4;
-    int index = binarySearch(arr, key);
-    cout << "Key found at index: " << index << endl;
-  
-
-    return 0;
+int main(){
+    vector<int>arr={1,2,3,4,5,6,7};
+    int search=6;
+    int index = binarySearch(arr,search);
+    cout<<"index of search value-> "<< index+1;
 }
