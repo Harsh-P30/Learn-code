33a5e50d1de8702a0c08aba20079d253a32f350a
Your Name <you@example.com>
2025-05-08 17:32:39 +0530

    Add explanation to How to Insert values at Head

--- LearnNewTings/ll2.cpp
+++ LearnNewTings/ll2.cpp
@@ -16,34 +16,35 @@ public:
     }
 };
 
-void InsertAtHead(Node *&head, int d)   // Insert at Head;
+void InsertAtHead(Node *&head, int d) // Insert at Head;
 // Node *&head → a reference to a pointer to the head node.
-//Takes a reference to a pointer head (Node *&head) → this allows the function to modify the actual head pointer in the calling code.
-//Takes an integer d → this is the data value to insert.
+// Takes a reference to a pointer head (Node *&head) → this allows the function to modify the actual head pointer in the calling code.
+// Takes an integer d → this is the data value to insert.
 {
     // new Node create
     Node *temp = new Node(d);
-    //Calls the Node constructor with value d. Dynamically allocates memory using new. temp now points to this new node.
+    // Calls the Node constructor with value d. Dynamically allocates memory using new. temp now points to this new node.
     temp->next = head;
-    //Sets the next pointer of the new node (temp) to point to the current head node.
+    // Sets the next pointer of the new node (temp) to point to the current head node.
     head = temp;
-    //Makes head point to the new node (temp).
+    // Makes head point to the new node (temp).
 }
 
-void print(Node *&head)  // take the Head 
+void print(Node *&head) // take the Head
 {
-    Node *temp = head;  // new node create put head into it
-    while (temp != NULL)  // temp != NULL
+    Node *temp = head;   // new node create put head into it
+    while (temp != NULL) // temp != NULL
     {
-        cout << temp->data<<" -> ";  // print data of temp 
-        temp = temp->next; // move temp to next 
-    }   
+        cout << temp->data << " -> "; // print data of temp
+        temp = temp->next;            // move temp to next
+    }
 }
 
-int main(){
-    Node* node1 = new Node(10);  // create a Node ** mandatory
-    Node* head = node1;  //  set just created node as Head
-    InsertAtHead(head, 12);  // call function giving argument as Head and value 
+int main()
+{
+    Node *node1 = new Node(10); // create a Node ** mandatory
+    Node *head = node1;         //  set just created node as Head
+    InsertAtHead(head, 12);     // call function giving argument as Head and value
     InsertAtHead(head, 15);
     print(head);
 }
